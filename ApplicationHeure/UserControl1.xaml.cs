d0faf48283ac85a48ca82be69eda26afa0d91835
AxelRigal <37696088+AxelRigal@users.noreply.github.com>
2020-06-02 10:37:17 +0200

    first commit

---
+++ ApplicationHeure/UserControl1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace ApplicationHeure
+{
+    /// <summary>
+    /// Logique d'interaction pour UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(dispatcher_timer);
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Start();
+            
+        }
+
+        private void dispatcher_timer(object sender, EventArgs e)
+        {
+            Heure.Text = DateTime.Now.ToString("HH:mm:ss");
+        }
+
+    }
+}
